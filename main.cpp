f884779ecc635ca710ab296150d0aefbe7a88d99
Aluno <aluno@ifc.blumenau.ifc.edu.br>
2024-04-22 16:40:25 -0300

    main loop logic

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <random>
 
 const int STARFIELD_SIZE = 10;
+const int STAR_SIZE = 5;
+const int HEIGHT = 600;
+const int WIDTH = 600;
+const int SPEED = 10;
 
 // Random number generation
 std::random_device rd;
@@ -17,41 +21,50 @@ int rand_num(int start, int end) {
 
 class Star {
     public:
-    float x, y;
+    float x, y, z;
 
     Star() {
-        this->x = rand_num(0,500);
-        this->y = rand_num(0,500);
+        this->x = rand_num(0,WIDTH);
+        this->y = rand_num(0,HEIGHT);
     }
 };
 
+//Array of Star()
 std::array<Star*, STARFIELD_SIZE> starfield;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(600, 600), "Window!");
+    sf::RenderWindow window(sf::VideoMode(HEIGHT, WIDTH), "Window!");
 
     for (int i = 0; i < STARFIELD_SIZE; i++){
         starfield[i] = new Star();
     }
 
-    window.clear();
-    for (int i = 0; i < STARFIELD_SIZE; i++){
-        sf::CircleShape starshape(5.f);
-        starshape.setFillColor(sf::Color::White);
-        starshape.setPosition((starfield[i]->x), (starfield[i]->y));
-        window.draw(starshape);
-    }
-    window.display();
     
+
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed){
                 window.close();
+            } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)){
+            window.close();
+            }
+        } 
+        
+        
+        starfield[0]->x+=0.01f;
+
+        window.clear();
+        for (int i = 0; i < STARFIELD_SIZE; i++){
+            sf::CircleShape starshape(STAR_SIZE);
+            starshape.setFillColor(sf::Color::White);
+            starshape.setPosition((starfield[i]->x), (starfield[i]->y));
+            window.draw(starshape);
         }
+        window.display();
     }
     return 0;
 }
